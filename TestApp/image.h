9899ad91b685cbe22c50ce3ec59e21ae83c6e5bc
Ryaith <19347142+Ryaith@users.noreply.github.com>
2018-11-29 16:25:29 -0500

    Update image.h
    Doxygen comments

--- TestApp/image.h
+++ TestApp/image.h
@@ -19,12 +19,22 @@ class image : public QObject
 {
     Q_OBJECT
 private:
+/** The label that the image is displayed in */
     QLabel* label;
 
 public:
+/**
+  @brief This class takes a given QQuickWidget and sets up a label to call an image refresh to load the picture name
+  @param parent The quickwidget to set the QLabel's parent
+  @author Stacey Gunderson, Alison Lee
+  **/
     explicit image(QQuickWidget *parent = nullptr);
 
 public slots:
+/**
+ * @brief Loads the image name in the singleton class picturename to the widget's label and displays the image or an error if no image exists
+ * @author Stacey Gunderson, Alison Lee
+ */
     void update();
 };
 
