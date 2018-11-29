635c84d2678928b042e3efec810f16e82d4e471f
Ryaith <19347142+Ryaith@users.noreply.github.com>
2018-11-29 16:53:13 -0500

    Update social.h

--- TestApp/social.h
+++ TestApp/social.h
@@ -26,20 +26,38 @@ class social: public QObject
     Q_OBJECT
     //Creates a social media browser in the widget given
 private:
-    QWebEngineView *wv; //The widget to use to display social media
-    QPushButton *reloadbutton; //The button that takes refresh clicks
+/**The webview widget pointer*/
+    QWebEngineView *wv; 
+/**The button that takes refresh clicks*/
+    QPushButton *reloadbutton;
+/**The CSS style to use for textbox */
     const QString style = "background-color: #A3C1DA; color: white;";
+/**The CSS style to use for buttons */
     QString stylebutton = "background-color: white; color: black";
+/**The textbox that takes the tumblr username */
     QLineEdit *urlbox;
+/**The url to load for tumblr pages on reload */
     QString url;
 
 public:
+/**
+  @brief Constructor that takes the given parent widget and displays a social media blog inside it, can be updated on button click
+  @author Stacey Gunderson, Alison Lee
+  @return A social object
+  @param QQuickWidget parent widget to display the social media blog in
+ **/
         social(QQuickWidget*parent = nullptr);
 
 public slots:
+/**
+  @brief Upon button click signal, reloads the url to refresh what is on the page
+  @author Stacey Gunderson, Alison Lee
+  @return void
+ **/
     void on_reload();
 
 signals:
+/** A signal that gets sent to on_reload to reload the social media*/
     void reload();
 };
 
